846b9cd1903b877e33f5c51531d80b576de13ba0
Stephen Dicks <stephen@starswan.com>
2015-10-03 10:32:19 +0100

    converted league table retrieval to use get params

--- tables.sh
+++ tables.sh
@@ -1,5 +1,6 @@
 filename=/tmp/league.tables.$$.html
-crossname=/tmp/cross.tables.$$.html
-wget -q -O $filename "http://phptest.mvhs.co.uk/live2011/org.php?org=Herts CA" --post-file=league.tables.params
+wget -q -O $filename "http://phptest.mvhs.co.uk/live2011/org.php?org=Herts%20CA&season=2014_2015&x4_report=division"
 python tables.py $filename
-wget -q -O $crossname "http://phptest.mvhs.co.uk/live2011/org.php?org=Herts%20CA&season=2014_2015&report=cross"
+rm $filename
+#crossname=/tmp/cross.tables.$$.html
+#wget -q -O $crossname "http://phptest.mvhs.co.uk/live2011/org.php?org=Herts%20CA&season=2014_2015&report=cross"
