5c108ae1deda3aaedaebf930ba92a5083298e2fa
Stephen Dicks <stephen@starswan.com>
2015-10-04 18:10:15 +0100

    more progress towards site parsing by keeping cookies

--- eacu.py
+++ eacu.py
@@ -1,15 +1,47 @@
-import urllib, HTMLParser
+import urllib2, HTMLParser, cookielib
 
 from BeautifulSoup import BeautifulSoup
+import requests
 
 site = "http://www.c4results.org.uk"
 siteurl = "http://www.c4results.org.uk/chess/php/"
-thisurl = "http://www.c4results.org.uk/chess/php/index.php?DBVersion=2014-15"
+top_url = "http://www.c4results.org.uk/chess/php/index.php"
+dbversion = { 'DBVersion': '2014-15' }
 
 eacu_teams = ['1st Team', 'U160']
 
-handle = urllib.urlopen(thisurl)
-top_page = BeautifulSoup(handle.read())
+class SiteAccessor(object):
+  def __init__(self):
+    self.cookies = cookielib.CookieJar()
+    self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookies))
+  def follow_url(self, url):
+    self.opener.open(url)
+  def load_url(self, base):
+    if base.find('?') >= 0:
+      url = base + '&'
+    else:
+      url = base + '?'
+
+    for p in dbversion:
+      url = url + p + '=' + dbversion[p]
+    webpage = self.opener.open(url)
+    top_page = BeautifulSoup(webpage.read())
+    return top_page
+
+accessor = SiteAccessor()
+
+def process_team(link):
+   print link['href'], link.contents
+   #league_page = BeautifulSoup(urllib.urlopen(site + link['href']).read())
+   #print league_page
+
+def start_new():
+  resp = requests.get(thisurl, params=dbversion)
+  print resp.cookies
+  top_page = BeautifulSoup(resp.text)
+  return top_page
+
+top_page = accessor.load_url(top_url)
 #print top_page
 for league in top_page.findAll('tr'):
   columns = league.findAll('td')
@@ -19,10 +51,21 @@ for league in top_page.findAll('tr'):
         print bit.contents
       print
       href = columns[1].contents[0]['href']
-      page = BeautifulSoup(urllib.urlopen(site + href).read())
-      href = page.findAll('td')[0].contents[0]['href']
-      eacupage = BeautifulSoup(urllib.urlopen(siteurl + href).read())
-      for link in eacupage.findAll('a'):
-        for team in eacu_teams:
-           if link.contents[0].endswith(team):
-              print link['href'], link.contents
+      print 'First Href', href
+      #page = BeautifulSoup(urllib2.urlopen(site + href).read())
+      page = accessor.load_url(site + href)
+      rows = page.findAll('tr')
+      for row in rows:
+         cells = row.findAll('td')
+         if len(cells) > 0:
+            anchors = cells[0].findAll('a')
+            if len(anchors) > 0:
+               if anchors[0].contents[0].startswith('EACU County'):
+                  href = anchors[0]['href']
+                  print 'Second Href', href
+                  #eacupage = BeautifulSoup(urllib2.urlopen(siteurl + href).read())
+                  eacupage = accessor.load_url(siteurl + href)
+                  for link in eacupage.findAll('a'):
+                     for team in eacu_teams:
+                        if link.contents[0].endswith(team):
+                           process_team(link)
