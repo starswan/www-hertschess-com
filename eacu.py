ccb25465ae7f261fe4a60b9c3e38ec7b4b4e53be
Stephen Dicks <stephen@starswan.com>
2015-10-04 22:20:56 +0100

    main structure complete

--- eacu.py
+++ eacu.py
@@ -1,7 +1,7 @@
 import urllib2, HTMLParser, cookielib
 
 from BeautifulSoup import BeautifulSoup
-import requests
+#import requests
 
 site = "http://www.c4results.org.uk"
 siteurl = "http://www.c4results.org.uk/chess/php/"
@@ -35,23 +35,22 @@ def process_team(link):
    #league_page = BeautifulSoup(urllib.urlopen(site + link['href']).read())
    #print league_page
 
-def start_new():
-  resp = requests.get(thisurl, params=dbversion)
-  print resp.cookies
-  top_page = BeautifulSoup(resp.text)
-  return top_page
+#def start_new():
+#  resp = requests.get(thisurl, params=dbversion)
+#  print resp.cookies
+#  top_page = BeautifulSoup(resp.text)
+#  return top_page
 
 top_page = accessor.load_url(top_url)
-#print top_page
 for league in top_page.findAll('tr'):
   columns = league.findAll('td')
   if len(columns) > 0:
     if columns[0].contents[0].startswith('EACU'):
-      for bit in columns:
-        print bit.contents
-      print
+      #for bit in columns:
+      #  print bit.contents
+      #print
       href = columns[1].contents[0]['href']
-      print 'First Href', href
+      #print 'First Href', href
       #page = BeautifulSoup(urllib2.urlopen(site + href).read())
       page = accessor.load_url(site + href)
       rows = page.findAll('tr')
@@ -62,10 +61,31 @@ for league in top_page.findAll('tr'):
             if len(anchors) > 0:
                if anchors[0].contents[0].startswith('EACU County'):
                   href = anchors[0]['href']
-                  print 'Second Href', href
-                  #eacupage = BeautifulSoup(urllib2.urlopen(siteurl + href).read())
+                  #print 'Second Href', href
                   eacupage = accessor.load_url(siteurl + href)
                   for link in eacupage.findAll('a'):
                      for team in eacu_teams:
                         if link.contents[0].endswith(team):
-                           process_team(link)
+                           print team, link['href']
+                           #league_page = BeautifulSoup(urllib.urlopen(site + link['href']).read())
+                           league_page = accessor.load_url(site + link['href']).findAll('table')[0]
+                           #print league_page
+                           header_data = league_page.findAll('th')
+                           print [h.contents for h in header_data]
+                           header_dict = { 'TEAM':1 }
+                           index = 0
+                           for header in header_data:
+                             if len(header.contents) > 0:
+                               header_dict[header.contents[0]] = index
+                             #print header.contents
+                             index = index + 1
+                           
+                           rows = league_page.findAll('tr')
+                           for row in rows[1:6]:
+                              cells = row.findAll('td')
+                              #print [c.contents for c in cells]
+                              for h in header_dict:
+                                 index = header_dict[h]
+                                 print h, cells[index]
+                              print
+                           print
