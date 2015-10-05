b535b1fa3e439c664e79bfb72a57235f74d57819
Stephen Dicks <stephen@starswan.com>
2015-10-05 17:27:34 +0100

    converted into a function

--- parse.py
+++ parse.py
@@ -1,22 +1,8 @@
 import sys, BeautifulSoup
 from utils import commaSep
 
-division = sys.argv[1]
-table = BeautifulSoup.BeautifulSoup(file(division + '.html').read())
-
-header_data = table.findAll('th')
-
 HEADER_TRANS = { 'TEAM':'Team Name', 'PLAYED':'P', 'WON':'W', 'DRAWN':'D', 'LOST':'L', 
                 'GAMES':'For','POINTS':'Ps', 'POSITION':'Position', 'DIVISION':'Division'}
-
-header_dict = { 'TEAM':1, 'POSITION':8, 'DIVISION':9 }
-index = 0
-for header in header_data:
-   if len(header.contents) > 0:
-      header_dict[header.contents[0]] = index
-   index = index + 1
-
-print commaSep([HEADER_TRANS[h] for h in header_dict])
 
 def celldata(cells, index):
   if index == 1:
@@ -26,11 +12,27 @@ def celldata(cells, index):
   else:
     return cells[index].contents[0].replace('&#189;','.5')
 
-index = 1
-for row in table.findAll('tr')[1:6]:
-  cells = row.findAll('td')
-  cells.append(str(index))
-  cells.append(division)
-  data = [celldata(cells, header_dict[h]) for h in header_dict]
-  print commaSep(data)
-  index = index + 1
+def parse(division, table):
+  header_data = table.findAll('th')
+
+  header_dict = { 'TEAM':1, 'POSITION':8, 'DIVISION':9 }
+  index = 0
+  for header in header_data:
+    if len(header.contents) > 0:
+       header_dict[header.contents[0]] = index
+    index = index + 1
+  print commaSep([HEADER_TRANS[h] for h in header_dict])
+
+  index = 1
+  for row in table.findAll('tr')[1:6]:
+    cells = row.findAll('td')
+    cells.append(str(index))
+    cells.append(division)
+    data = [celldata(cells, header_dict[h]) for h in header_dict]
+    print commaSep(data)
+    index = index + 1
+
+division = sys.argv[1]
+table = BeautifulSoup.BeautifulSoup(file(division + '.html').read())
+
+parse(division, table)
