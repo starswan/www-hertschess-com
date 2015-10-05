34af56e88e3a85af89a893d8785d8c55a1981beb
Stephen Dicks <stephen@starswan.com>
2015-10-05 17:22:58 +0100

    parse now producing correct csv

--- parse.py
+++ parse.py
@@ -1,28 +1,36 @@
 import sys, BeautifulSoup
 from utils import commaSep
 
-table = BeautifulSoup.BeautifulSoup(file(sys.argv[1]).read())
+division = sys.argv[1]
+table = BeautifulSoup.BeautifulSoup(file(division + '.html').read())
 
 header_data = table.findAll('th')
 
-header_dict = { 'TEAM':1 }
+HEADER_TRANS = { 'TEAM':'Team Name', 'PLAYED':'P', 'WON':'W', 'DRAWN':'D', 'LOST':'L', 
+                'GAMES':'For','POINTS':'Ps', 'POSITION':'Position', 'DIVISION':'Division'}
+
+header_dict = { 'TEAM':1, 'POSITION':8, 'DIVISION':9 }
 index = 0
 for header in header_data:
    if len(header.contents) > 0:
       header_dict[header.contents[0]] = index
    index = index + 1
 
-#def commaSep(array):
-#  return ",".join(['"' + a.strip() + '"' for a in array])
-#
-print commaSep([h for h in header_dict])
+print commaSep([HEADER_TRANS[h] for h in header_dict])
 
 def celldata(cells, index):
   if index == 1:
     return cells[index].findAll('a')[0].contents[0]
+  elif index > 7:
+    return cells[index]
   else:
     return cells[index].contents[0].replace('&#189;','.5')
 
+index = 1
 for row in table.findAll('tr')[1:6]:
   cells = row.findAll('td')
-  print commaSep([celldata(cells, header_dict[h]) for h in header_dict])
+  cells.append(str(index))
+  cells.append(division)
+  data = [celldata(cells, header_dict[h]) for h in header_dict]
+  print commaSep(data)
+  index = index + 1
